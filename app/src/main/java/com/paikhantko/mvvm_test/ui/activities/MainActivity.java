302c5f25ce8627f68d7c012e937c79724aca4918
Pai Khant Ko <kopaikhant@gmail.com>
2020-07-01 11:02:19 +0630

    project first commit

---
+++ app/src/main/java/com/paikhantko/mvvm_test/ui/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.paikhantko.mvvm_test.ui.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.paikhantko.mvvm_test.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
